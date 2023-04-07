929062de14ad899694d0f99dff9fa6be9169537b
Sujith S <sujiths@Sujith-MacBook-Air.local>
2023-04-07 17:53:18 +0530

    CRUD Application Demo

---
+++ src/main/java/com/sujith/chat/EmployeeManagementSystemThymeleafCrudApplication.java
@@ -0,0 +1,13 @@
+package com.sujith.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSystemThymeleafCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSystemThymeleafCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sujith/chat/EmployeeManagementSystemThymeleafCrudApplicationTests.java
@@ -0,0 +1,14 @@
+package com.sujith.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class EmployeeManagementSystemThymeleafCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
